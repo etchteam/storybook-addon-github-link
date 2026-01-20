6fb2c66cd78c983ff5df07ac46827e8339c62228
Dan Webb <dan@etch.co>
2026-01-20 17:58:14 +0000

    feat!: Storybook v10 support
    - Update imports to use new storybook/* paths
    - Switch from webpack5 to vite framework
    - Convert local-preset.js to ESM
    - Update tsup config for ESM-only output
    - Add storybook ^10.0.0 as peer dependency
    - Bump Node requirement to 22

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,10 @@
 const config = {
   stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    '@storybook/addon-essentials',
-    '@storybook/addon-webpack5-compiler-swc',
-    './local-preset.js',
+    '@storybook/addon-docs',
+    import.meta.resolve('./local-preset.js'),
   ],
-  framework: {
-    name: '@storybook/react-webpack5',
-    options: {}
-  },
-  docs: {
-    autodocs: true
-  }
+  framework: '@storybook/react-vite',
 };
 
 export default config;
